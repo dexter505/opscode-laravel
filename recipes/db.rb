63bfd1692e976945c79a42a6a6d159f11f6bbb73
dexter505 <dexter505@users.noreply.github.com>
2015-05-14 21:43:58 -0700

    Update db.rb

--- recipes/db.rb
+++ recipes/db.rb
@@ -26,6 +26,15 @@ bash 'run composer to grab extensions' do
   EOH
 end
 
+# Run jocopo user auth plugin install first
+bash 'insert_db_laravel_authentication_extension' do
+  user 'root'
+  cwd "/var/www/#{node['laravel']['name']}"
+  code <<-EOH
+  yes | php artisan authentication:install
+  EOH
+end
+
 # Run artisan migrate to setup the database and schema, then seed it
 bash 'insert_db_laravel' do
   user 'root'
@@ -35,12 +44,3 @@ bash 'insert_db_laravel' do
   php artisan db:seed --env=development
   EOH
 end
-
-bash 'insert_db_laravel_authentication_extension' do
-  user 'root'
-  cwd "/var/www/#{node['laravel']['name']}"
-  code <<-EOH
-  yes | php artisan authentication:install
-  EOH
-end
-
