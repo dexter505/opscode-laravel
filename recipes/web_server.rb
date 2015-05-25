8af36b9fb9e6564027f8a89542ebfc916b5d4be2
dexter505 <dexter505@users.noreply.github.com>
2015-05-24 20:05:29 -0700

    Update web_server.rb

--- recipes/web_server.rb
+++ recipes/web_server.rb
@@ -5,35 +5,6 @@
 # Copyright 2013, Mathias Hansen
 #
 
-# Makes sure apt is up to date
-include_recipe "apt"
-
-# Add repositories
-apt_repository "php55" do
-	uri "http://ppa.launchpad.net/ondrej/php5/ubuntu"
-	distribution node['lsb']['codename']
-	components ["main"]
-	keyserver "keyserver.ubuntu.com"
-	key "E5267A6C"
-end
-
-apt_repository 'apache2' do
-	uri 'http://ppa.launchpad.net/ondrej/apache2/ubuntu'
-	distribution node['lsb']['codename']
-	components ['main']
-	keyserver 'keyserver.ubuntu.com'
-	key 'E5267A6C'
-end
-
-# Install Apache & PHP
-include_recipe "openssl"
-include_recipe "apache2"
-apache_module "authz_default" do
-  enable false
-end
-
-include_recipe "php"
-
 # Install xdebug
 include_recipe "chef-php-extra::xdebug"
 
