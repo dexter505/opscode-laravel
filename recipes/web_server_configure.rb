baafb6ed0164b34001a83724e835159852f4677e
dexter505 <dexter505@users.noreply.github.com>
2015-05-25 18:02:08 -0700

    Update web_server_configure.rb

--- recipes/web_server_configure.rb
+++ recipes/web_server_configure.rb
@@ -8,31 +8,5 @@
 # Install mcrypt
 include_recipe "chef-php-extra::module_mcrypt"
 
-#not sure why this doesn't happen in the mcrypt recipe...
-node[:deploy].each do |app_name, deploy|
-
-  script "enable_mcrypt" do
-    interpreter "bash"
-    user "root"
-    cwd "#{deploy[:deploy_to]}/current"
-    code <<-EOH
-    sudo php5enmod mcrypt
-    EOH
-  end
-end
-
 # Install xdebug
 include_recipe "chef-php-extra::xdebug"
-
-# Install composer
-node[:deploy].each do |app_name, deploy|
-  script "install_composer" do
-    interpreter "bash"
-    user "root"
-    cwd "#{deploy[:deploy_to]}/current"
-    code <<-EOH
-    curl -sS https://getcomposer.org/installer | php
-    sudo mv composer.phar /usr/local/bin/composer
-    EOH
-  end
-end
